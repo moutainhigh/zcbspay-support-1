64bef15b5fd56f60426c9ddb3ec31fca22133862
guojia <dmcguojia@qq.com>
2017-05-16 11:39:14 +0800

    新增签名验签加密打包模块

---
+++ support/support-signature-simple/src/main/java/org/support/signature/simple/App.java
@@ -0,0 +1,13 @@
+package org.support.signature.simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
